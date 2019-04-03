66c3921ed7a0a892e46005c9111833950f3088a0
Victor Veytsman <vityavv@gmail.com>
2019-04-02 22:38:37 -0400

    BROKEN CODE: Add methods to interact with users
    this is not yet connected to main.go or tested because I have to go to
    sleep. Will connect everything tomorrow. Sorry!

--- db.go
+++ db.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "github.com/mattn/go-sqlite3"
+	"golang.org/x/crypto/bcrypt"
 	"database/sql"
 	"html/template"
 )
@@ -9,7 +10,7 @@ import (
 type Guide struct {
 	Subject, Title string
 	Content template.HTML
-	Delta string
+	Delta, Creator string
 }
 
 var db *sql.DB
@@ -20,11 +21,15 @@ func DBinit() error {
 	if err != nil {
 		return err
 	}
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS guides (id INTEGER PRIMARY KEY AUTOINCREMENT, subject TEXT, title TEXT UNIQUE, content TEXT, delta TEXT)")
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS guides (id INTEGER PRIMARY KEY AUTOINCREMENT, subject TEXT, title TEXT UNIQUE, content TEXT, delta TEXT, creator TEXT)")
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, password TEXT)")
 	return err
 }
-func DBinsert(subject, title, content, delta string) error {
-	_, err := db.Exec("INSERT INTO guides (subject, title, content, delta) VALUES (?, ?, ?, ?)", subject, title, content, delta)
+func DBclose() {db.Close()}
+
+// Study guide methods {{{
+func DBinsert(subject, title, content, delta, creator string) error {
+	_, err := db.Exec("INSERT INTO guides (subject, title, content, delta, creator) VALUES (?, ?, ?, ?)", subject, title, content, delta, creator)
 	return err
 }
 func DBedit(title, content, delta string) error {
@@ -32,38 +37,87 @@ func DBedit(title, content, delta string) error {
 	return err
 }
 func DBget(title string) (Guide, bool, error) {
-	rows, err := db.Query("SELECT content, subject, delta FROM guides WHERE title = ?", title)
+	rows, err := db.Query("SELECT content, subject, delta, creator FROM guides WHERE title = ?", title)
 	if err != nil {
 		return Guide{}, false, err
 	}
 	defer rows.Close()
 	if rows.Next() {
-		var content, subject, delta string
-		err = rows.Scan(&content, &subject, &delta)
+		var content, subject, delta, creator string
+		err = rows.Scan(&content, &subject, &delta, &creator)
 		if err != nil {
 			return Guide{}, false, err
 		}
-		return Guide{subject, title, template.HTML(content), delta}, false, nil
+		return Guide{subject, title, template.HTML(content), delta, creator}, false, nil
 	}
 	return Guide{}, true, nil
 }
 func DBgetAll() ([]Guide, error) {
-	rows, err := db.Query("SELECT subject, title, content, delta FROM guides")
+	rows, err := db.Query("SELECT subject, title, content, delta, creator FROM guides")
 	if err != nil {
 		return []Guide{}, err
 	}
 	defer rows.Close()
 	returnval := make([]Guide, 0)
 	for rows.Next() {
-		var c, t, s, d string
-		err = rows.Scan(&s, &t, &c, &d)
+		var c, t, s, d, cr string
+		err = rows.Scan(&s, &t, &c, &d, &cr)
 		if err != nil {
 			return []Guide{}, err
 		}
-		returnval = append(returnval, Guide{s, t, template.HTML(c), d})
+		returnval = append(returnval, Guide{s, t, template.HTML(c), d, cr})
 	}
 	return returnval, nil
 }
+// }}}
 
-func DBclose() {db.Close()}
-
+// User methods: {{{
+func DBcreateUser(username, password string) error {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(username), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	_, err := db.Exec("INSERT INTO users (username, password) VALUES (?, ?)", username, string(hashed))
+	if err != nil {
+		return err
+	}
+}
+func DBlogIn(username, password string) error {
+	rows, err := db.Query("SELECT password FROM users WHERE username = ?", username)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+	if rows.Next() {
+		var hashed string
+		err = rows.Scan(&hashed)
+		if err != nil {
+			return err
+		}
+		err = bcrypt.CompareHashAndPassword(hashed, password)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	return errors.New("User not found")
+}
+// TODO: Combine title and subject into one thing! Never change independently and are a PITA to handle separately
+func DBgetOfUser(username string) []struct{Title, Subject string} { //returns title/subjects of guides the user has created
+	rows, err := db.Query("SELECT title, subject FROM guides WHERE creator = ?", username)
+	if err != nil {
+		return []struct{Title, Subject string}{}, err
+	}
+	defer rows.Close()
+	returnval := make([]struct{Title, Subject string}, 0)
+	for rows.Next() {
+		var title, subject string
+		err = rows.Scan(&title, &subject)
+		if err != nil {
+			return []struct{Title, Subject string}{}, err
+		}
+		returnval = append(returnval, struct{Title, Subject string}{titleGuide{s, t, template.HTML(c), d, cr}, subject})
+	}
+	return returnval, nil
+}
+// }}}
